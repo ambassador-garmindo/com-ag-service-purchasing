c6815ccbd48ad0191a64a0e7e75cfd824fe8d329
lyzbeth <lyzbeth@users.noreply.github.com>
2021-01-19 08:12:31 +0700

    purchasing update stockopname

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20210108061334_garment_stock_opnames_table.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20210108061334_garment_stock_opnames_table.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210108061334_garment_stock_opnames_table")]
+    partial class garment_stock_opnames_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
